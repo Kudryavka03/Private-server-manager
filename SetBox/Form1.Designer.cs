11c243f48ac9708f2c25ee180beeeca6f1640a80
Otaku <Otaku@DESKTOP-MEDRL1V>
2021-06-02 23:43:15 +0800

    添加项目文件。

---
+++ SetBox/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace SetBox
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.UID_TEXT = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Token_Text = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 44);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1330, 616);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 12);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(39, 20);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "UID:";
+            // 
+            // UID_TEXT
+            // 
+            this.UID_TEXT.Location = new System.Drawing.Point(50, 9);
+            this.UID_TEXT.Name = "UID_TEXT";
+            this.UID_TEXT.Size = new System.Drawing.Size(92, 27);
+            this.UID_TEXT.TabIndex = 2;
+            this.UID_TEXT.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "读取Unit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(399, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "提交Unit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Token:";
+            // 
+            // Token_Text
+            // 
+            this.Token_Text.Location = new System.Drawing.Point(199, 9);
+            this.Token_Text.Name = "Token_Text";
+            this.Token_Text.Size = new System.Drawing.Size(106, 27);
+            this.Token_Text.TabIndex = 6;
+            this.Token_Text.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(487, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "模糊搜索：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(564, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(478, 27);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1049, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "上一个结果";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1130, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "下一个结果";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1358, 674);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Token_Text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UID_TEXT);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Main";
+            this.Text = "Unit Editor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox UID_TEXT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Token_Text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ SetBox/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SetBox
+{
+    public partial class Main : Form
+    {
+
+        public Main()
+        {
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+            InitializeComponent();
+            Type dgvType = this.dataGridView1.GetType();
+            PropertyInfo pi = dgvType.GetProperty("DoubleBuffered",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(this.dataGridView1, true, null);
+        }
+        DataTable dt = new DataTable();
+        string jsonstr;
+        public string ToJson(DataTable dt)
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            javaScriptSerializer.MaxJsonLength = Int32.MaxValue; //取得最大数值
+            ArrayList arrayList = new ArrayList();
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();  //实例化一个参数集合
+                foreach (DataColumn dataColumn in dt.Columns)
+                {
+                    if (dataColumn.ColumnName.ToString() == "name")
+                    { 
+                        dictionary.Add(dataColumn.ColumnName, dataRow[dataColumn.ColumnName].ToString());
+                    }
+                    else
+                    {
+                        dictionary.Add(dataColumn.ColumnName, dataRow[dataColumn.ColumnName]);
+                    }
+                }
+                arrayList.Add(dictionary); //ArrayList集合中添加键值
+            }
+            return javaScriptSerializer.Serialize(arrayList);  //返回一个json字符串
+        }
+        public   DataTable ToDataTable(string json)
+        {
+            DataTable dataTable = new DataTable();  //实例化
+            DataTable result;
+            try
+            {
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                javaScriptSerializer.MaxJsonLength = Int32.MaxValue; //取得最大数值
+                ArrayList arrayList = javaScriptSerializer.Deserialize<ArrayList>(json);
+                if (arrayList.Count > 0)
+                {
+                    foreach (Dictionary<string, object> dictionary in arrayList)
+                    {
+                        if (dictionary.Keys.Count<string>() == 0)
+                        {
+                            result = dataTable;
+                            return result;
+                        }
+                        if (dataTable.Columns.Count == 0)
+                        {
+                            foreach (string current in dictionary.Keys)
+                            {
+                                dataTable.Columns.Add(current, dictionary[current].GetType());
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        foreach (string current in dictionary.Keys)
+                        {
+                            dataRow[current] = dictionary[current];
+                        }
+
+                        dataTable.Rows.Add(dataRow); //循环添加行到DataTable中
+                    }
+                }
+
+                //dataGridView1.DataSource = dt;
+            }
+            catch
+            {
+                MessageBox.Show("呀！返回的数据好像不太对呢......");
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+            result = dataTable;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            return result;
+
+        }
+
+
+
+        public void JsonToTable(string jsondata)
+    {
+        dt =ToDataTable(jsondata);
+    }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            try
+            {
+                UID_TEXT.Text = System.IO.File.ReadAllText(System.Windows.Forms.Application.StartupPath + "\\user.txt");
+                Token_Text.Text = System.IO.File.ReadAllText(System.Windows.Forms.Application.StartupPath + "\\token.txt");
+            }
+            catch
+            {
+
+            }
+            //dt.Columns.Add("Unit ID", typeof(Int32));
+            //dt.Columns.Add("角色名称", typeof(String));
+            //dt.Columns.Add("等级", typeof(Int32));
+            //dt.Columns.Add("星级", typeof(Int32));
+            //dt.Columns.Add("RANK", typeof(Int32));
+            //dt.Columns.Add("装备1", typeof(Int32));
+            //dt.Columns.Add("装备2", typeof(Int32));
+            //dt.Columns.Add("装备3", typeof(Int32));
+            //dt.Columns.Add("装备4", typeof(Int32));
+            //dt.Columns.Add("装备5", typeof(Int32));
+            //dt.Columns.Add("装备6", typeof(Int32));
+            //dt.Columns.Add("专武等级", typeof(Int32));
+            //dt.Columns.Add("UB", typeof(Int32));
+            //dt.Columns.Add("技能1", typeof(Int32));
+            //dt.Columns.Add("技能2", typeof(Int32));
+            //dt.Columns.Add("EX技能", typeof(Int32));
+            //dt.Columns.Add("好感度", typeof(Int32));
+        }
+        private async void getdata(string uid , string token)
+        {
+            dt.Clear();
+            var web_url = "https://s1.pcrbot.fun:2087/api/getbox?uid="+uid+"&token="+token;
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(web_url);
+            var body = await response.Content.ReadAsByteArrayAsync();
+            //MessageBox.Show(Encoding.UTF8.GetString(body));
+            var body1 = Encoding.UTF8.GetString(body).Replace("\r", string.Empty);
+            //MessageBox.Show("OK!");
+            //JObject obj = Newtonsoft.Json.Linq.JObject.Parse(body1);
+            if (Encoding.UTF8.GetString(body) == "[]")
+            {
+                MessageBox.Show("UID或Token不对哦，再检查看看？");
+                button1.Enabled = true;
+            }
+            else
+            {
+                JsonToTable(Encoding.UTF8.GetString(body));
+                dataGridView1.DataSource = dt;
+                //dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+        }
+        public DataTable GetDgvToTable(DataGridView dgv)
+        {
+            DataTable dt = new DataTable();
+            int check;
+
+            // 列强制转换
+            for (int count = 0; count < dgv.Columns.Count; count++)
+            {
+                DataColumn dc = new DataColumn(dgv.Columns[count].Name.ToString());
+                if (dgv.Columns[count].Name.ToString() == "name")
+                {
+                    dt.Columns.Add(dgv.Columns[count].Name.ToString());
+                }
+                else
+                {
+                    dt.Columns.Add(dgv.Columns[count].Name.ToString(), typeof(Int32));
+                }
+            }
+
+            // 循环行
+            for (int count = 0; count < dgv.Rows.Count; count++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int countsub = 0; countsub < dgv.Columns.Count; countsub++)
+                {
+                    try
+                    {
+                        dr[countsub] = Convert.ToInt32(dgv.Rows[count].Cells[countsub].Value);
+                    }
+                    catch
+                    {
+                        dr[countsub] = Convert.ToString(dgv.Rows[count].Cells[countsub].Value);
+                    }
+
+                    //dr[countsub] = dgv.Rows[count].Cells[countsub].Value;
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            getdata(UID_TEXT.Text, Token_Text.Text);
+
+        }
+        public string DataTableToJsonWithJsonNet(DataTable table)
+        {
+            string JsonString = string.Empty;
+            JsonString = JsonConvert.SerializeObject(table);
+            return JsonString;
+        }
+        private async void postdata(string mainbody)
+        {
+            //dt.Clear();
+            var web_url = "https://s1.pcrbot.fun:2087/api/setbox";//?uid=" + uid + "&token=" + token;
+            HttpClient httpClient = new HttpClient();
+            HttpContent content = new ByteArrayContent(Encoding.UTF8.GetBytes(mainbody));
+            content.Headers.ContentType=new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            HttpResponseMessage response = await httpClient.PostAsync(web_url,content);
+            var body = await response.Content.ReadAsByteArrayAsync();
+            var body1 = Encoding.UTF8.GetString(body).Replace("\r", string.Empty);
+            MessageBox.Show("提交请求已发送，点击确定以同步数据");
+            getdata(UID_TEXT.Text, Token_Text.Text);
+            button2.Enabled = true;
+            button1.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = false;
+            DataTable dt = new DataTable();
+            dt = GetDgvToTable(dataGridView1);
+            string poststr;
+            poststr = "uid=" + UID_TEXT.Text + "&token=" + Token_Text.Text + "&data=" + DataTableToJsonWithJsonNet(dt);
+            //MessageBox.Show(DataTableToJsonWithJsonNet(dt));
+            postdata(poststr);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!(textBox1.Text=="")) {
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    if (dataGridView1.Rows[i].Cells[1].Value.ToString().Contains(textBox1.Text))
+                    {
+                        //dataGridView1.CurrentCell = dataGridView1.Rows[1].Cells[0];
+                        //dataGridView1.Rows[i].Selected = false;
+                        //找到不匹配行 ，隐藏掉
+                        //dataGridView1.Rows[i].Selected = true;
+                        dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells[0];
+                        dataGridView1.ClearSelection();
+                        dataGridView1.CurrentCell = dataGridView1.Rows[i].Cells[1];
+                        return;
+                    }
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!(textBox1.Text == ""))
+                {
+                    for (int i = dataGridView1.CurrentRow.Index - 1; i > -1; i--)
+                    {
+                        if (dataGridView1.Rows[i].Cells[1].Value.ToString().Contains(textBox1.Text))
+                        {
+                            //dataGridView1.CurrentCell = dataGridView1.Rows[1].Cells[0];
+                            //dataGridView1.Rows[i].Selected = false;
+                            //找到不匹配行 ，隐藏掉
+                            //dataGridView1.Rows[i].Selected = true;
+                            dataGridView1.ClearSelection();
+
+                            dataGridView1.CurrentCell = dataGridView1.Rows[i].Cells[1];
+                            return;
+                        }
+                        //MessageBox.Show(i.ToString());
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!(textBox1.Text == ""))
+                {
+                    for (int i = dataGridView1.CurrentRow.Index + 1; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[1].Value.ToString().Contains(textBox1.Text))
+                        {
+                            //dataGridView1.CurrentCell = dataGridView1.Rows[1].Cells[0];
+                            //dataGridView1.Rows[i].Selected = false;
+                            //找到不匹配行 ，隐藏掉
+                            //dataGridView1.Rows[i].Selected = true;
+                            dataGridView1.ClearSelection();
+                            dataGridView1.CurrentCell = dataGridView1.Rows[i].Cells[1];
+                            return;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            dataGridView1.Width = this.Width - 44;
+            dataGridView1.Height  = this.Height - 97;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
